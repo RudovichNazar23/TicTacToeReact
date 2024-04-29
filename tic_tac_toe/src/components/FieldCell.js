206bd4ac929f77fa4d1b212ebc2cb877264940df
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-04-29 19:48:41 +0200

    added functionality for setting circle or cross to field cells

--- tic_tac_toe/src/components/FieldCell.js
+++ tic_tac_toe/src/components/FieldCell.js
@@ -1,11 +1,14 @@
-import { useState } from "react";
-
-export default function FieldCell({ value }){
-    const [fieldValue, setFieldValue] = useState(value);
+export default function FieldCell({ intilalValue, rowIndex, cellIndex, setCellValue}){
+    const onClick = (event) => {
+        setCellValue(rowIndex, cellIndex);
+    };
 
     return (
-        <div className="d-flex flex-column align-items-center justify-content-center p-5 border">
-            {fieldValue}
+        <div className="d-flex flex-column align-items-center justify-content-center p-5 border"
+             style={{width: "150px", height: "150px"}}
+             onClick={onClick}
+        >
+            {intilalValue}
         </div>
     );
 };
--- tic_tac_toe/src/components/GameField.js
+++ tic_tac_toe/src/components/GameField.js
@@ -1,26 +1,49 @@
 import FieldCell from "./FieldCell";
 import { useState } from "react";
 
-
 export default function GameField(){
     const intialFieldState = [
         ["", "", ""],
         ["", "", ""],
         ["", "", ""]
     ];
+    const crossIcon = <img src="/close.png" />
+    const circleIcon = <img src="/dry-clean.png" />
 
     const [fieldState, setFieldState] = useState(intialFieldState);
+    const [currentIcon, setCurrentIcon] = useState(true);
+
+    const onSetCellValue = (rowIndex, cellIndex) => {
+        if (fieldState[rowIndex][cellIndex]) return;
+        if(currentIcon){
+            fieldState[rowIndex][cellIndex] = crossIcon;
+            setCurrentIcon(false);
+            setFieldState(fieldState);
+        }
+        else{
+            fieldState[rowIndex][cellIndex] = circleIcon;
+            setCurrentIcon(true);
+            setFieldState(fieldState);
+        }
+    };
 
     return (
         <div className="d-flex flex-column align-items-center justify-content-center">
             {
                 fieldState.map(
-                    (row) => 
+                    (row, rowIndex) => 
                         <div className="d-flex flex-row align-items-center justify-content-between"
                              key={Math.random() * 10}
                         > 
                             {
-                                row.map((cellValue) => <FieldCell value={cellValue} key={Math.random() * 10}/>)
+                                row.map((cellValue, cellIndex) => <FieldCell intilalValue={cellValue} 
+                                                                  key={Math.random() * 10}
+                                                                  rowIndex={rowIndex}
+                                                                  cellIndex={cellIndex}
+                                                                  setCellValue={onSetCellValue}
+                                                                  
+                                                        />
+                                )
                             } 
                         </div>
                 )
